63a9fb11a825f9be962dca140c9eac9b93288084
WanyoikeNgugi <jamesngugi967@gmail.com>
2023-11-20 22:32:43 +0300

    initial setup

--- calculator.js
+++ calculator.js
@@ -12,13 +12,34 @@ document.getElementById("calc").addEventListener("click", function (event) {
     } else if (operation === "CE") {
       screenValue = screenValue.slice(0, -1);
     } else if (operation === "equals") {
+      inverseTrigFunc();
+      calculateTrig();
       calculate();
+    } else if (
+      operation === "sin⁻¹" ||
+      operation === "cos⁻¹" ||
+      operation === "tan⁻¹"
+    ) {
+      appendToDisplay(`${operation}(`);
     } else if (
       operation === "sin" ||
       operation === "cos" ||
       operation === "tan"
     ) {
       appendToDisplay(`${operation}(`);
+    } else if (
+      operation === "sinh⁻¹" ||
+      operation === "cosh⁻¹" ||
+      operation === "tanh⁻¹"
+    ) {
+      appendToDisplay(`${operation}(`);
+    } else if (operation === "√" || operation === "∛") {
+      appendToDisplay(`${operation}(`);
+    } else if (operation === "xⁿ") {
+      let x = prompt("Enter the base:");
+      let n = prompt("Enter the exponent:");
+      screenValue = Math.pow(x, n).toString();
+      screen.value = screenValue;
     } else {
       screenValue += value;
     }
@@ -31,7 +52,12 @@ function appendToDisplay(value) {
   if (
     value.includes("sin(") ||
     value.includes("cos(") ||
-    value.includes("tan(")
+    value.includes("tan(") ||
+    value.includes("sinh(") ||
+    value.includes("cosh(") ||
+    value.includes("tanh(") ||
+    value.includes("√(") ||
+    value.includes("∛(")
   ) {
     screenValue += value;
   } else {
@@ -45,7 +71,30 @@ function calculate() {
     screenValue = screenValue.replace(/sin/g, "Math.sin");
     screenValue = screenValue.replace(/cos/g, "Math.cos");
     screenValue = screenValue.replace(/tan/g, "Math.tan");
-
+    screenValue = screenValue.replace(/√/g, "Math. sqrt");
+    screenValue = screenValue.replace(/∛/g, "Math. cbrt");
+    screenValue = eval(screenValue);
+    screen.value = screenValue;
+  } catch (error) {
+    screen.value = "Error";
+  }
+}
+function calculateTrig() {
+  try {
+    screenValue = screenValue.replace(/sin⁻¹/g, "Math.asin");
+    screenValue = screenValue.replace(/cos⁻¹/g, "Math.acos");
+    screenValue = screenValue.replace(/tan⁻¹/g, "Math.atan");
+    screenValue = eval(screenValue);
+    screen.value = screenValue;
+  } catch (error) {
+    screen.value = "Error";
+  }
+}
+function inverseTrigFunc() {
+  try {
+    screenValue = screenValue.replace(/sinh⁻¹/g, "Math.asinh");
+    screenValue = screenValue.replace(/cosh⁻¹/g, "Math.acosh");
+    screenValue = screenValue.replace(/tanh⁻¹/g, "Math.atanh");
     screenValue = eval(screenValue);
     screen.value = screenValue;
   } catch (error) {
